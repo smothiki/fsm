1682cb188e34a772c183774395bb2f089c108484
smothiki <sivaram.mothiki@gmail.com>
2016-10-11 10:01:33 -0700

    generic example

--- examples/alternate.go
+++ examples/alternate.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/looplab/fsm"
 )
 
@@ -11,56 +12,54 @@ func main() {
 	fsm := fsm.NewFSM(
 		"idle",
 		fsm.Events{
-			{Name: "scan", Src: []string{"idle"}, Dst: "scanning"},
-			{Name: "working", Src: []string{"scanning"}, Dst: "scanning"},
-			{Name: "situation", Src: []string{"scanning"}, Dst: "scanning"},
-			{Name: "situation", Src: []string{"idle"}, Dst: "idle"},
-			{Name: "finish", Src: []string{"scanning"}, Dst: "idle"},
+			{Name: "stage1", Src: []string{"idle"}, Dst: "success"},
+			{Name: "stage1", Src: []string{"idle"}, Dst: "failure"},
+			{Name: "stage2", Src: []string{"error", "updating"}, Dst: "success"},
 		},
 		fsm.Callbacks{
-			"scan": func(e *fsm.Event) {
+			"stage1": func(e *fsm.Event) {
+				e.Dst = "success"
 				fmt.Println("after_scan: " + e.FSM.Current())
+
+				//e.Err = errors.New("type")
 			},
-			"working": func(e *fsm.Event) {
+			"stage2": func(e *fsm.Event) {
 				fmt.Println("working: " + e.FSM.Current())
 			},
 			"situation": func(e *fsm.Event) {
 				fmt.Println("situation: " + e.FSM.Current())
-			},
-			"finish": func(e *fsm.Event) {
-				fmt.Println("finish: " + e.FSM.Current())
 			},
 		},
 	)
 
 	fmt.Println(fsm.Current())
 
-	err := fsm.Event("scan")
+	err := fsm.Event("stage1")
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	fmt.Println("1:" + fsm.Current())
 
-	err = fsm.Event("working")
+	err = fsm.Event("stage2")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println("2:" + fsm.Current())
+	// fmt.Println("2:" + fsm.Current())
+	//
+	// err = fsm.Event("situation")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 
-	err = fsm.Event("situation")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println("3:" + fsm.Current())
-
-	err = fsm.Event("finish")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println("4:" + fsm.Current())
+	// fmt.Println("3:" + fsm.Current())
+	//
+	// err = fsm.Event("finish")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	//
+	// fmt.Println("4:" + fsm.Current())
 
 }
